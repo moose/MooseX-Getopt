747717044102ba1feba0134bdbcb7aeb3d2d1adb
Karen Etheridge <ether@cpan.org>
2013-08-16 15:55:34 -0700

    spelling (RT#87780)

--- lib/MooseX/Getopt/Meta/Attribute.pm
+++ lib/MooseX/Getopt/Meta/Attribute.pm
@@ -56,7 +56,7 @@ now it works for those who might need such a feature.
 
 You should rarely need to explicitly set the attribute metaclass. It is much
 preferred to simply provide a trait (a role applied to the attribute
-metaclass), which allows other code to futher modify the attribute by applying
+metaclass), which allows other code to further modify the attribute by applying
 additional roles.
 
 Therefore, you should first try to do this:
