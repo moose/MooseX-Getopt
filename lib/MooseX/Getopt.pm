75272b67d024974bec2112823b94bef1e92874d2
Karen Etheridge <ether@cpan.org>
2018-09-23 15:54:54 -0700

    amend documentation to clarify that exit happens after the print_usage_text call, not in it

--- lib/MooseX/Getopt.pm
+++ lib/MooseX/Getopt.pm
@@ -250,8 +250,9 @@ options (true if any of these options were passed on the command line).
 =method C<print_usage_text>
 
 This method is called internally when the C<help_flag> state is true.
-It prints the text from the C<usage> object (see above) to C<STDOUT> and then the
-program terminates normally.  You can apply a method modification (see
+It prints the text from the C<usage> object (see above) to C<STDOUT>
+(and then after this method is called, the
+program terminates normally).  You can apply a method modification (see
 L<Moose::Manual::MethodModifiers>) if different behaviour is desired, for
 example to include additional text.
 
